9b8afa86e3b380ffe5022250cb7122ab739b70f2
Kevin McQuown <kevinmcquown@me.com>
2015-06-13 09:50:36 -0500

    Initial Commit

---
+++ GetDibs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GetDibs
+//
+//  Created by Kevin McQuown on 6/13/15.
+//  Copyright (c) 2015 Windy City Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
